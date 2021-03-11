3787433a6d84e81131fe05dae78e26a647a038bd
nyanshiba <31783332+nyanshiba@users.noreply.github.com>
2021-03-11 16:35:04 +0900

    より柔軟なログ整形の正規表現・Webhook内容の設定を可能にした

--- readmcchat.ps1
+++ readmcchat.ps1
@@ -10,31 +10,53 @@ param (
 # ユーザ設定
 $Profiles =
 @{
+    # /etc/crontab: @reboot user pwsh readmcchat.ps1 -Name CBWSurvival
     "CBWSurvival" =
+    @{
+        # latest.logのパス
+        latestLogPath = "~/Servers/CBWSurvival/logs/latest.log"
+
+        # Webhook URL (Discord, Slack等)
+        hookUrl = "https://discordapp.com/api/webhooks/XXXXXXXXXX"
+
+        # 一致する行の条件 (Minecraftサーバへの接続/切断, プレイヤーのチャットに一致)
+        Pattern = "Server thread/INFO\]: (<.*|.*(joined|left) the game)"
+
+        # 一致した行の整形
+        Formatter =
+        ({
+            return [Regex]::Replace($_, "^.*]: (.*)$", { $args.Groups[1].Value })
+        })
+    }
+    "GetPlayerCountry" =
     @{
         latestLogPath = "~/Servers/CBWSurvival/logs/latest.log"
         hookUrl = "https://discordapp.com/api/webhooks/XXXXXXXXXX"
+        Pattern = "logged"
+        Formatter =
+        ({
+            # プレイヤーのIPv4/IPv6アドレスを取得
+            $ipaddr = [Regex]::Replace($_, "^.*(\[/\[|\[/)((\w+:)+\w+|(\w+\.)+\w+).*$", { $args.Groups[2].Value })
+
+            # ipinfo.ioで国を取得
+            return (Invoke-WebRequest -Uri "https://ipinfo.io/$ipaddr/json" | ConvertFrom-Json).country
+        })
     }
 }
 
-<#
-想定したlatest.log
-[04:18:54] [Server thread/INFO]: <yucaf> ﾗｼﾞｬ！
-[16:25:26] [Server thread/INFO]: yucaf joined the game
-[16:27:45] [Server thread/INFO]: yucaf lost connection: Disconnected
-[16:27:45] [Server thread/INFO]: yucaf left the game
-[16:25:26] [User Authenticator #141/INFO]: UUID of player yucaf is XXX-XXX-XXX-XXX
-[16:25:26] [Server thread/INFO]: yucaf[/みせられないよ！] logged in with entity id 60248913 at (-24.24919487158972, 126.0, 373.40783442617834)
-[20:06:30] [Server thread/INFO]: [Server] The server shuts down after 10 seconds.
-[20:06:40] [Server thread/INFO]: Stopping the server
-[20:06:40] [Server thread/INFO]: Stopping server
-[20:06:40] [Server thread/INFO]: Saving players
-[20:06:40] [Server thread/INFO]: Saving worlds
-[20:06:40] [Server thread/INFO]: Saving chunks for level 'world'/minecraft:the_end
-[20:06:40] [Server thread/INFO]: ThreadedAnvilChunkStorage (DIM1): All chunks are saved
-...
-#>
-# tmux new-session -ds readmcchat pwsh ~/Repos/mcchat-discord/readmcchat.ps1
-Get-Content -Wait -Tail 0 -Path $Profiles.$Name.latestLogPath | Select-String -Pattern "Server thread/INFO\]: (<.*|.*(joined|left) the game)" | ForEach-Object {
-    Invoke-RestMethod -Uri $Profiles.$Name.hookUrl -Method Post -Headers @{ "Content-Type" = "application/json" } -Body ([System.Text.Encoding]::UTF8.GetBytes(([PSCustomObject]@{ content = "$(($_ -split ']: ')[1])"; username = "$Name" } | ConvertTo-Json -Depth 1)))
+# ファイル監視
+Get-Content -Wait -Tail 0 -Path $Profiles.$Name.latestLogPath | Select-String -Pattern $Profiles.$Name.Pattern | ForEach-Object {
+    # Webhook
+    Invoke-RestMethod -Uri $Profiles.$Name.hookUrl -Method Post -Headers @{
+        "Content-Type" = "application/json"
+    } -Body (
+        [System.Text.Encoding]::UTF8.GetBytes(
+            (
+                [PSCustomObject]@{
+                    content = "$(Invoke-Command -ScriptBlock $Profiles.$Name.Formatter)"
+                    username = "$Name"
+                } | ConvertTo-Json -Depth 1
+            )
+        )
+    )
 }
